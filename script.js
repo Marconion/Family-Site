b6f8b41d7850b7043305c391cf57d551f450b619
Marconion <zujovic.marko@gmail.com>
2023-05-08 19:07:36 +0200

    Ubacene slike

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const navLinks = document.querySelectorAll(".nav-link");
-const menuToggle = document.getElementById("navbarNavAltMarkup");
+const menuToggle = document.getElementById("navbarNav");
 const bsCollapse = new bootstrap.Collapse(menuToggle, { toggle: false });
 navLinks.forEach((l) => {
   l.addEventListener("click", () => {
