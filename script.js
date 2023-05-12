0837073004450d21857947136753caffa07060a7
Marconion <zujovic.marko@gmail.com>
2023-05-12 10:25:11 +0200

    small changes

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
+$(document).ready(function () {
+  window.onload = function () {
+    $("body").hide().fadeIn(1000);
+  };
+});
+
 const navLinks = document.querySelectorAll(".nav-link");
 const menuToggle = document.getElementById("navbarNav");
 const bsCollapse = new bootstrap.Collapse(menuToggle, { toggle: false });
